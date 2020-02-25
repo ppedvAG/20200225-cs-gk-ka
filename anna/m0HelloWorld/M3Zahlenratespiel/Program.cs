01defa2515cd78ef3010b4ab6b1fd6951823bd36
Anna453 <gaba.anna@yahoo.com>
2020-02-25 17:21:11 +0100

    Notes

---
+++ anna/m0HelloWorld/M3Zahlenratespiel/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace M3Zahlenratespiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Zahlenratespiel");
+            Random generator = new Random();
+            int zufallszahl = generator.Next(1, 4);
+            int ratemalzahl = 0;
+            Console.WriteLine("Erraten Sie eine int-Zahl zwischen 1 und 3: ");
+
+
+            do
+            {
+                Console.WriteLine("Geben Sie eine Zahl ein: ");
+                ratemalzahl = int.Parse(Console.ReadLine());
+
+                if (ratemalzahl == zufallszahl)
+                {
+                    Console.WriteLine("Super");
+
+                }
+
+                else if (ratemalzahl < zufallszahl)
+                { Console.WriteLine("ihre zahl Kleiner"); }
+
+                else { Console.WriteLine("ihre zahl Größer"); }
+
+
+            } while (ratemalzahl != zufallszahl);
+
+
+        }
+    }
+}

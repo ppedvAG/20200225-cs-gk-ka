6679213626093c89d25c1eb7c4386bb75a730bd2
JanKarlsruhe <janmariusehmann@web.de>
2020-02-26 15:41:48 +0100

    Neu

---
+++ jan/CSharpKursKA/DClass/MyClassWProps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DClass
+{
+    public class MyClassWProps
+    {
+        // fields
+        // manuelle Eingabe von Feldern beim Benutzen von Props unnötig, wenn wir die get und set Methoden nicht änden
+        // private string firstname;
+        // private string lastname;
+
+        // props
+        // Konvention: großgeschrieben
+        // snippet: prob tab tab
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        // constructor
+        // snippet: ctor tab tab
+        public MyClassWProps(string fn, string ln)
+        {
+            Firstname = fn;
+            Lastname = ln;
+        }
+    }
+}

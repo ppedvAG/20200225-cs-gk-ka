fa46da0043fbd689bfebfe8e5359d9b3d4b10bc4
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-02-26 15:41:15 +0100

    classes demo done

---
+++ vadzim/m0HelloWorld/DClass/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DClass
+{
+    public class MyClass
+    {
+        // fields
+        // sind immer private
+        // Konvention: klein geschrieben
+        private string strMyField1;
+        private int intMyField2;
+        // props
+        // constructor
+        // methods
+        public void SetFields(string arg1, int arg2)
+        {
+            strMyField1 = arg1;
+            intMyField2 = arg2;
+        }
+        public string GetFields()
+        {
+            // return "Grüße aus MyClass"; // Default-Werte werden zurückgegeben
+            return strMyField1 + intMyField2; // Default-Werte werden zurückgegeben
+
+        }
+        // destructor
+    }
+}

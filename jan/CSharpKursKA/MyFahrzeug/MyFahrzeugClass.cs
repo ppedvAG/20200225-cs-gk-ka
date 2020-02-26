b77e451050e975f593db27d033823847bd507440
JanKarlsruhe <janmariusehmann@web.de>
2020-02-26 17:02:39 +0100

    Neu

---
+++ jan/CSharpKursKA/MyFahrzeug/MyFahrzeugClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFahrzeug
+{
+    public class MyFahrzeugClass
+    {
+        // properties
+        public string Name { get; set; }
+        public int MaximalGeschwindigkeit { get; set; }
+        public int Preis { get; set; }
+        public int AktuelleGeschwindigkeit { get; set; }
+        public string Zustand { get; set; }
+        
+
+        // constructor
+        public MyFahrzeugClass(string Nm, int MG, int Pr, int AG, string Zs)
+        {
+            Name = Nm;
+            MaximalGeschwindigkeit = MG;
+            Preis = Pr;
+            AktuelleGeschwindigkeit = AG;
+            Zustand = Zs;
+        }
+
+        // methoden
+        public int Beschleunige(int wert1)
+        {
+            if((AktuelleGeschwindigkeit + wert1) < MaximalGeschwindigkeit)
+            {
+                AktuelleGeschwindigkeit += wert1;
+            }
+            return AktuelleGeschwindigkeit;
+        }
+
+        public string StarteMotor()
+        {
+            Zustand = "fahrend";
+            return Zustand;
+        }
+
+        public string StoppeMotor()
+        {
+            Zustand = "stehend";
+            return Zustand;
+        }
+
+        public string BeschreibeMich()
+        {
+            return $"Das Fahrzeug ist ein {Name}, die Maximalgeschwindigkeit ist {MaximalGeschwindigkeit}, " +
+                $"der Preis ist {Preis}, die aktuelle Geschwindigkeit ist {AktuelleGeschwindigkeit}, der aktuelle Zustand ist {Zustand}";
+        }
+
+    }
+}

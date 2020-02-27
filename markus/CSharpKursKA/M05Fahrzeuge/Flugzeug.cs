9614a52afa624bc5e3a80b2062f6388f647aefbb
Markus Galli <galli.markus@outlook.com>
2020-02-27 12:26:42 +0100

    new

---
+++ markus/CSharpKursKA/M05Fahrzeuge/Flugzeug.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M05Fahrzeuge
+{
+    public class Flugzeug : FahrzeugLib
+    {
+        public int AnzahlTriebwerke { get; set; }
+        public Flugzeug(string strName, int MaxV, int iPreis, int AktuellV,int anzahlTriebwerke) : base(strName, MaxV, iPreis, AktuellV)
+        {
+            AnzahlTriebwerke = anzahlTriebwerke;
+        }
+
+        public override string BeschreibeMich()
+        {
+            return base.BeschreibeMich() + $"\nAnzahl Triebwerke: {AnzahlTriebwerke}";
+        }
+    }
+}

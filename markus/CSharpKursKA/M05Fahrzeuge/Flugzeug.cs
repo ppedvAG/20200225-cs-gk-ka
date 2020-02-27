49268dc47497173b5f75f372b2a3977df4ab116a
Markus Galli <galli.markus@outlook.com>
2020-02-27 17:34:12 +0100

    new

--- markus/CSharpKursKA/M05Fahrzeuge/Flugzeug.cs
+++ markus/CSharpKursKA/M05Fahrzeuge/Flugzeug.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +7,31 @@ using System.Threading.Tasks;
 
 namespace M05Fahrzeuge
 {
-    public class Flugzeug : FahrzeugLib,IRollbar
+    public class Flugzeug : FahrzeugLib, IRollbar, IEnumerable
     {
         public int AnzahlTriebwerke { get; set; }
-        public int AnzahlRäder { get ; set ; }
+        public int AnzahlRäder { get; set; }
+
+        public List<string> PassagierListe { get; set; }
+
+        // Damit können wir die Klasse iterierbar machen
+        public string this[int i]
+        {
+            get
+            {
+                return PassagierListe[i];
+            }
+            set
+            {
+                PassagierListe[i] = value;
+            }
+        }
 
         public Flugzeug(string strName, int MaxV, int iPreis, int anzahlTriebwerke) : base(strName, MaxV, iPreis)
         {
             AnzahlTriebwerke = anzahlTriebwerke;
             this.AnzahlRäder = 10;
+            PassagierListe = new List<string>() { "Anna", "Kevin-Pascal", "Hannes" };
         }
 
         public override string BeschreibeMich()
@@ -26,6 +43,18 @@ namespace M05Fahrzeuge
         {
             Console.WriteLine("Da war ein Vogel im Weg.");
             this.AnzahlTriebwerke--;
+        }
+        // Durch IEnumerable verlangte Methode
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in PassagierListe)
+            {
+                // return ((IEnumerable)PassagierListe).GetEnumerator();
+                // Rendite, Ertrag
+                // Mittels yield - kann in jedem Durchlauf der Schleife ein Wert zurückgegeben werden,
+                // ohne dass die Methode danach abgebrochen wird.
+                yield return item;
+            }
         }
     }
 }

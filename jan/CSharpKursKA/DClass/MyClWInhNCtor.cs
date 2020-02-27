a691733d7c9f2440ba480e84bba664b4e9b3c14c
JanKarlsruhe <janmariusehmann@web.de>
2020-02-27 11:06:08 +0100

    Neu

---
+++ jan/CSharpKursKA/DClass/MyClWInhNCtor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DClass
+{
+    public class MyClWInhNCtor : MyClassWProps
+    {
+        // neue Prop in der abeleiteten Klasse
+        public string Wohnort { get; set; }
+
+        // ctor generiert mit: Cursor auf die Klasse > Schrauenzieher > generate constructor
+        public MyClWInhNCtor(string fn, string ln, string wohnort) : base(fn, ln)
+        {
+            Wohnort = wohnort;
+        }
+
+        public string BeschreibeMich()
+        {
+            return $@"Prop Firstname: {Firstname}
+            Prop Lastname: {Lastname}
+            Prop Wohnort: {Wohnort}";
+        }
+        
+
+    }
+}
---
+++ jan/CSharpKursKA/DClass/MyClassWInheritance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DClass
+{
+    // die Methode Random.Next() wollen wir erben und anpassen
+    public class MyClassWInheritance : Random
+    {
+        public int NextMaxInclusive(int min, int max)
+        {
+            return base.Next(min, max + 1);
+        }
+    }
+}

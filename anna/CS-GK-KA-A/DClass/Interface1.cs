3a550b6a44c0f045c98ef8f88bc032de017cbf63
Anna453 <gaba.anna@yahoo.com>
2020-02-27 15:39:22 +0100

    interfaces

---
+++ anna/CS-GK-KA-A/DClass/Interface1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLibrary
+{
+
+    //Konvention : Interfacenamen fangen mit I an 
+    interface IMyInterface
+    {
+        int FancyLevel { get; set; }
+        int ChnageFancyLevel(int fl, int FancyLevel);
+        string SayHalloInYourWay(); 
+
+
+
+
+    }
+}
--- anna/CS-GK-KA-A/DClass/MyClWInhNCtor.cs
+++ anna/CS-GK-KA-A/DClass/MyClWInhNCtor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DClass
+namespace DLibrary
 {
     public class MyClWInhNCtor : MyClassWProps
 

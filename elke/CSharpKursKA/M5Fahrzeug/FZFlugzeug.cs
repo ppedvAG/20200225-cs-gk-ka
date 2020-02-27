f7b6b860dc2d5172d8b5853593a5e1529165a4ea
ElkeWaldenmeier <elke.waldenmeier@gmx.de>
2020-02-27 17:34:08 +0100

    new

--- elke/CSharpKursKA/M5Fahrzeug/FZFlugzeug.cs
+++ elke/CSharpKursKA/M5Fahrzeug/FZFlugzeug.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +7,27 @@ using System.Threading.Tasks;
 
 namespace M5Fahrzeug
 {
-    public class FZFlugzeug : M5FahrzeugClass, IRollbar
+    public class FZFlugzeug : M5FahrzeugClass, IRollbar, IEnumerable
     {
         public string Flugzeugtyp { get; set; }
         public int AnzahlRäder { get; set; }
+
+        public List<string> Passagierliste { get; set; }
+
+        //damit können wir die Klasse iterierbar machen (wie ein Array)
+        public  string this[int i]
+        {
+            get { return Passagierliste[i]; }
+            set { Passagierliste[i] = value; }
+        }
+
 
         public FZFlugzeug(string Name, int MaxG, int Preis, int AktG, string Zustand, string flugzeugtyp) : base(Name, MaxG, Preis, AktG, Zustand)
         {
             this.Flugzeugtyp = flugzeugtyp;
             this.AnzahlRäder = 10;
+            Passagierliste = new List<string>() { "Anna", "Kevin", "Hannes" };
+
         }
 
         public override string BeschreibeMich()
@@ -27,6 +40,18 @@ namespace M5Fahrzeug
             Console.WriteLine("Du hat einen Vogel übersehen.");
             this.AnzahlRäder -= 2;//Räder werden um 2 verringert
 
+        }
+        //durch Ienumerable 
+        public IEnumerator GetEnumerator()
+        {
+            //return ((IEnumerable)Passagierliste).GetEnumerator();
+            foreach (var item in Passagierliste)
+            {
+                // yield = Rendite / Ertrag
+                // Mittels yield  kann in jedem Durchlauf der Schleie ein Wert zurückgegeben werden,
+                // ohne dass die Methode danach abgebrochen wird
+                yield return item;
+            }
 
         }
     }

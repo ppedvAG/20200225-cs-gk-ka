040e13e67bc062a1a97432a0ca866c4c2e9178df
ElkeWaldenmeier <elke.waldenmeier@gmx.de>
2020-02-28 16:00:05 +0100

    new

---
+++ elke/CSharpKursKA/DEntityFrCore/DEntityFrCore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DEntityFrCore
+
+
+{
+    class DEntityFrCore
+
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
